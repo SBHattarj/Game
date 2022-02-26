245ccbcb54049fb89d31aaf4cea70a6f3a2f9efe
SBHattarj <subhambhsubhambh@gmail.com>
2022-02-26 13:24:33 +0000

    fixed Vector

--- shapes/Vector.py
+++ shapes/Vector.py
@@ -1,5 +1,7 @@
 import numpy as np
-from typing import Iterable
+from numpy import dtype
+from numpy.typing import _SupportsArray, _NestedSequence
+from typing import Iterable, Any
 
 class Vector(np.ndarray):
     def __new__(cls, items: Iterable):
@@ -13,29 +15,17 @@ class Vector(np.ndarray):
     def sqr_length(self):
         return np.sum(np.square(self))
 
-    @property
-    def length(self):
-        return np.linalg.norm(self)
-
-    def dot(self, other: Iterable[float] | np.ndarray | 'Vector'):
-        Other = Vector(other)
-        len_def = len(self) - len(Other)
-        if len_def == 0:
-            return np.dot(self, Other)
-
-        if len_def > 0:
-            edited_other = type(self)([*Other] + [0] * len_def)
-            return np.dot(self, edited_other)
-
-        if len_def < 0:
-            edited_self = type(self)([*self] + [0] * (-len_def))
-            return np.dot(edited_self, Other)
-
-    def cross(self, other: Iterable[float] | np.ndarray | 'Vector'):
+    
+    
+    def cross(self, other: _SupportsArray[dtype[Any]] | _NestedSequence[_SupportsArray[dtype[Any]]] | Any | _NestedSequence[Any]):
         return np.cross(self, other)
 
     def normalize(self):
-        l2 = np.atleast_1d(self.length)
+        l2 = np.atleast_1d(np.linalg.norm(self))
+        if l2 == 0:
+            newVector = self.clone()
+            newVector[1] = 1
+            return newVector
         if l2 == 1: return self.clone()
         l2[l2==0] = 1
         normalized = self / l2
@@ -44,13 +34,19 @@ class Vector(np.ndarray):
 
     def set_length(self, length: float | Iterable[float] | np.ndarray | 'Vector'):
         return self.normalize() * length
+    
+    @property
+    def length(self):
+        return np.linalg.norm(self)
 
-    @length.setter
+    @length.setter 
     def length(self, length: float | Iterable[float] | np.ndarray | 'Vector'):
-        self[0], self[1] = self.set_length(length)
+        new_self = self.set_length(length)
+        self.dtype = new_self.dtype
+        self[:] = new_self
 
     def project(self, other: Iterable[float] | np.ndarray | 'Vector'):
-        return self * self.dot(other) / self.dot(self)
+        return self * self.dot(Vector(other)) / self.dot(self)
     
 class Vector2D(Vector):
     def __new__(cls, x: float, y: float):
@@ -91,5 +87,7 @@ class Vector3D(Vector):
     def z(self, value: float):
         return self[2]
 
-v = Vector3D(1, 2, 3)
-print(v.z)
+
+v = Vector([1, 2, 3])
+v.length = 6
+print(v)
